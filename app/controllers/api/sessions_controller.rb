282c2fc24b70cd014e2be657dca71dac1a1cc8e9
Chris Park <cpark2653@gmail.com>
2022-01-21 12:01:52 -0800

    added sessions controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,22 @@
+class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+    if @user
+      login!(@user)
+      render "api/users/show"
+    else
+      render json: {error: "Invalid Credentials"}
+    end
+  end
+
+  def destroy        
+    if current_user
+      logout!
+      render json: {}
+    else
+      render json: {error: "Not logged in", status: 400} 
+    end
+  end
+
+end
