b62503f8e30d3bfaac5a1fb977fc3e4c206f4a6e
Chris Park <cpark2653@gmail.com>
2022-01-22 10:16:03 -0800

    added logo handleclick to root

--- frontend/components/navbar/logo.jsx
+++ frontend/components/navbar/logo.jsx
@@ -2,8 +2,16 @@ import React from "react";
 
 class Logo extends React.Component {
 
+  constructor(){
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(e) {
+    
+  }
+
   render() {
-    return <div className="logo">
+    return <div className="logo" onClick={this.handleClick}>
       <img src="https://i.imgur.com/CsU8233.png" alt="" width="65px"/>
       <span className="logo-title">SomeTrails</span>
     </div>
