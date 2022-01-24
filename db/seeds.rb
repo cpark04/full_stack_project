3ad3b18d12e6a3cc8d3fe77faa544c088f53644c
Chris Park <cpark2653@gmail.com>
2022-01-24 13:02:17 -0800

    seeded data for park

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,8 @@
 User.destroy_all
 Park.destroy_all
 
-demo = User.create(fname: 'demo', lname: 'demo', email: 'demo@gmail.com', password: '12341234')
+demo = User.create!(fname: 'demo', lname: 'demo', email: 'demo@gmail.com', password: '12341234')
 
+sequoia = Park.create!(park_name: 'Yosemite National Park', description: "Want to find the best trails in Yosemite National Park for an adventurous hike or a family trip? AllTrails has 282 great hiking trails, biking trails, running trails and more. Enjoy hand-curated trail maps, along with reviews and photos from nature lovers like you.
+
+Ready for your next hike or bike ride? Explore one of 37 easy hiking trails in Yosemite National Park that are great for the whole family. Looking for a more strenuous hike? We've got you covered, with trails ranging from 6 to 46,030 feet in elevation gain. Whatever you have planned for the day, you can find the perfect trail for your next trip to Yosemite National Park.", latitude: 37.865101100000004, longitude: -119.54382256372995, country: 'United States of America', state: 'California', directions: 'https://goo.gl/maps/QkBbu9n1Mqe6mB7x7')
