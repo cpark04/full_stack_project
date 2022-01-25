6ab83297fcc0d66d30ad001dd7e57c806b8de88d
Chris Park <cpark2653@gmail.com>
2022-01-25 12:08:37 -0800

    fixed bug from breaking code

--- frontend/components/park/park.jsx
+++ frontend/components/park/park.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Breadcrumb from "./breadcrumb";
-import 'dotenv/config' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-import express from 'express'
 
 class Park extends React.Component {
 
