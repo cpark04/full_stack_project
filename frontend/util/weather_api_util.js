14292ef8ce37b6191e9c70b5f4e81b69fd8a4345
Chris Park <cpark2653@gmail.com>
2022-01-31 09:17:41 -0800

    fixed api window and added height to sliders

--- frontend/util/weather_api_util.js
+++ frontend/util/weather_api_util.js
@@ -1,6 +1,6 @@
 export const fetchWeather = (lat, long) => {
     return $.ajax({
         method:"GET",
-        url: `https://api.weatherapi.com/v1/forecast.json?key=26f26fa7dad24e648e761346222801&q=${lat},${long}&days=6&aqi=no&alerts=no`
+        url: `https://api.weatherapi.com/v1/forecast.json?key=${window.weatherAPI}&q=${lat},${long}&days=6&aqi=no&alerts=no`
     })
 }
