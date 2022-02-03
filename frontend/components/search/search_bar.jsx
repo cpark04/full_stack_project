5c4f2f03a103b2ff029e4240ce33bf22f701d5b4
Chris Park <cpark2653@gmail.com>
2022-02-02 21:25:28 -0800

    testing code;

--- frontend/components/search/search_bar.jsx
+++ frontend/components/search/search_bar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSelect } from "react-select-search";
+import SelectSearch from "react-select-search";
 
 const SearchBar = ({ options, value, multiple, disabled }) => {
   
@@ -7,12 +7,13 @@ const SearchBar = ({ options, value, multiple, disabled }) => {
   
   return <div className="search-container">
     <p>Find your next Trail</p>
-    <form className='search-form'>
+    <div className='search-form'>
       <input type="text" className="search-bar" placeholder="Search by park or trail name"/>
       <button className='search-button'>
         <img src="https://cdn-assets.alltrails.com/assets/packs/73160f92adaa640e4b1e.svg" alt="icon"></img>
       </button>
-    </form>
+    </div>
+
   </div>
 }
 
@@ -20,3 +21,16 @@ const SearchBar = ({ options, value, multiple, disabled }) => {
 
 
 export default SearchBar;
+// export default function SearchBar() {
+  
+//   const options = [
+//       {name: 'Swedish', value: 'sv'},
+//       {name: 'English', value: 'en'},
+//       {name: 'Summer', value: 'en'},
+//       {name: 'Frank', value: 'en'},
+//       {name: 'John', value: 'en'},
+//   ];
+//   return(
+//     <SelectSearch options={options} value="sv" name="language" placeholder="Choose your language" className="search-bar"/>
+//   )
+// }
